7c39bb10d758a2c0328fa3e00ce112b5cfd0e766
Graciela Diaz <gudiaz@me.com>
2016-09-27 19:38:47 -0400

    Adding login functionality

---
+++ js/login.js
@@ -0,0 +1,41 @@
+/*
+oooo                        o8o              
+`888                        `"'              
+ 888   .ooooo.   .oooooooo oooo  ooo. .oo.   
+ 888  d88' `88b 888' `88b  `888  `888P"Y88b  
+ 888  888   888 888   888   888   888   888  
+ 888  888   888 `88bod8P'   888   888   888  
+o888o `Y8bod8P' `8oooooo.  o888o o888o o888o 
+                d"     YD                    
+                "Y88888P'                    
+*/
+
+// Create an instance of the Facebook provider object:
+var provider = new firebase.auth.FacebookAuthProvider();
+
+//Authenticate with Firebase using the Facebook provider object.
+function login(){
+  //To sign in by redirecting to the sign-in page, call signInWithRedirect:
+  firebase.auth().signInWithRedirect(provider);
+}
+
+// The recommended way to get the current user is by setting an observer on the Auth object. By using an observer, you ensure that the Auth object isn't in an intermediate state—such as initialization—when you get the current user. 
+// When you use signInWithRedirect, the onAuthStateChanged observer waits until getRedirectResult resolves before triggering.
+
+firebase.auth().getRedirectResult().then(function(result) {    
+  // Save the User Name to Firebase
+  firebase.database().ref('user').push({
+    name: result.user.displayName
+  });
+
+  //Retrieve the User Name from Firebase
+  firebase.database().ref('user').on("child_added", function(snapshot){
+    console.log(snapshot.val().name);
+})
+    //console.log(result);
+    //console.log(result.user.displayName);
+});
+
+$('#login').on('click', function(){
+  login();
+});
--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyABtGUoDn3GvxXrfRA-bQKLrulCYCoHd_E",
+    authDomain: "littlebrother-67da5.firebaseapp.com",
+    databaseURL: "https://littlebrother-67da5.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "194837337033"
+  };
+  firebase.initializeApp(config);
+
+// Variable to reference the database
+var database = firebase.database();
+
+
+
 // This button clears the rows
 $('#btnClearRows').on('click', function(){
   counter = 0;
